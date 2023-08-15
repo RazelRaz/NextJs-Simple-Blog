210819619d3c87836b49b61c98e549b7de197cfa
RazelRaz <razel16dcm@gmail.com>
2023-08-15 14:29:13 +0600

    blog page

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,9 +1,26 @@
+'use client'
 
+import Link from 'next/link';
+import blogData from '../../data/blogData.json'
 
 const BlogPage = () => {
+
+
+
     return (
         <div>
             <h1>Blog Page</h1>
+            <div>
+                <ul>
+                    {blogData.map((post) => (
+                        <li key={post.id}>
+                            <Link href={`/blog/${post.id}`}>
+                            {post.title}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
